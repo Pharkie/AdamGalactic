a94884a67d89f077c3173369e908d38c145ea8bc
Adam Knowles <1413836+Pharkie@users.noreply.github.com>
2023-09-20 21:49:46 +0100

    freeze time

--- hottubtimemachine.py
+++ hottubtimemachine.py
@@ -1,4 +1,5 @@
 import utime
+import math
 from galactic import GalacticUnicorn
 from picographics import PicoGraphics, DISPLAY_GALACTIC_UNICORN as DISPLAY
 
@@ -19,6 +20,25 @@ height = GalacticUnicorn.HEIGHT
 
 gu.set_brightness(1)
 
+def freeze_time():
+    print("here")
+    # Get the current time in HH:MM:SS format
+    current_time = utime.localtime()
+    formatted_time = "{:02d}:{:02d}:{:02d}".format(current_time[3], current_time[4], current_time[5])
+    
+    # Update the TOP_TEXT variable
+    TOP_TEXT = formatted_time
+
+    graphics.set_pen(graphics.create_pen(int(BACKGROUND_COLOUR[0]), int(BACKGROUND_COLOUR[1]), int(BACKGROUND_COLOUR[2])))
+    graphics.clear()
+
+    graphics.set_pen(graphics.create_pen(int(MESSAGE_COLOUR[0]), int(MESSAGE_COLOUR[1]), int(MESSAGE_COLOUR[2])))
+    graphics.text(TOP_TEXT, 10, -1, -1, 0.4)
+    graphics.text(BOTTOM_TEXT, 0, 5, -1, 0.4)
+    
+    # Update the display
+    gu.update(graphics)
+
 while True:
     # Get the current time in HH:MM:SS format
     current_time = utime.localtime()
@@ -31,7 +51,8 @@ while True:
         gu.adjust_brightness(+0.01)
 
     if gu.is_pressed(GalacticUnicorn.SWITCH_BRIGHTNESS_DOWN):
-        gu.adjust_brightness(-0.01)
+        freeze_time()
+        break
 
     graphics.set_pen(graphics.create_pen(int(BACKGROUND_COLOUR[0]), int(BACKGROUND_COLOUR[1]), int(BACKGROUND_COLOUR[2])))
     graphics.clear()
@@ -44,5 +65,5 @@ while True:
     gu.update(graphics)
 
     # Pause for a moment (important or the USB serial device may fail)
-    utime.sleep(01)  # Adjust the sleep duration to 0.5 seconds for a faster colon flash
+    utime.sleep(0.1)  # Adjust the sleep duration to 0.5 seconds for a faster colon flash
 
